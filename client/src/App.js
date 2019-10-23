4ea86c101754c66b5043961bec9250b44b1336b3
heenabhansali <heenaj198@gmail.com>
2019-10-23 09:06:34 +0530

    add homepage

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Logo from './images/logo.png'
+import Locate from './images/1.png'
+import Restaurant from './images/2.png'
+import Order from './images/3.png'
+import Food from './images/4.png'
+import Location from './Location'
+
+
+function App() {
+    return (
+        <div>
+        <div class="bg">
+        <img class="logo" src={Logo} alt=""/>
+        <h1>Order food online from the best restaurants </h1>
+        <h2> Enter your delivery location to get started </h2>
+        
+        <Location />
+    </div>
+    <div class="flex-1">
+        <div> <img class="step" src={Locate} alt=""/>
+            <h3>Choose your location</h3>
+        </div>
+        <div><img class="step" src={Restaurant} alt=""/>
+            <h3>Choose Restaurant</h3>
+        </div>
+        <div><img class="step" src={Order} alt=""/>
+            <h3>Place Order</h3>
+        </div>
+        <div><img class="step" src={Food} alt=""/>
+            <h3>Food is on the way</h3>
+        </div>
+    </div>
+    </div>
+    )
+}
+
+export default App;
