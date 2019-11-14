71bf3fa044e2af516fa9cbe980fd3af139001af0
Sivaram Balakrishnan <sivaram@iyangi.com>
2019-11-14 10:50:43 +0530

    configure https

--- server/src/main.js
+++ server/src/main.js
@@ -2,9 +2,11 @@ const restaurantRoutes = require('./routes/restaurants.js')
 const userManagementRoutes = require('./routes/userManagement.js')
 const placesRoutes = require('./routes/places.js')
 const trackingRoutes = require('./routes/tracking.js')
+const fs = require('fs')
 
 const express = require('express')
 const Redis = require('ioredis')
+const https = require('https');
 const session = require('express-session')
 const RedisStore = require('connect-redis')(session)
 
@@ -12,6 +14,17 @@ const port = 8080
 const client = new Redis()
 
 const app = express()
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/tummypolice.iyangi.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/tummypolice.iyangi.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/tummypolice.iyangi.com/chain.pem', 'utf8');
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca
+};
+
 
 app.use(
   session({
@@ -41,4 +54,11 @@ app.use('/api/v1', userManagementRoutes)
 app.use('/api/v1', placesRoutes)
 app.use('/api/v1', trackingRoutes)
 
+
+const httpsServer = https.createServer(credentials, app)
+
 app.listen(port, () => console.log("gonna kill your hunger starting from port", port))
+
+httpsServer.listen(8088, () => {
+  console.log('HTTPS Server running on port 8088');
+})
