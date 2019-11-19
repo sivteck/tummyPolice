490a006ea03f3b78e9609e9e118a975ec1315a19
heenabhansali <heenaj198@gmail.com>
2019-11-19 12:39:51 +0530

    Modify index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import "./style.css"
 // import tryMap from './delivery executive app/tryMap.js'
 // import Login from './delivery executive app/Login.js'
 import App from "./App"
+import UseReducer from './UseReducer.js'
 // import Location from './Location'
 // import FormValidation from './FormValidation'
 // import Menu from './Menu'
@@ -13,7 +14,7 @@ import App from "./App"
 // import Map from './delivery executive app/Map.js'
 
 
-ReactDOM.render(<App />, document.getElementById("root"))
+// ReactDOM.render(<App />, document.getElementById("root"))
 // ReactDOM.render(<Menu />, document.getElementById('root'))
 // ReactDOM.render(<FormValidation />, document.getElementById('root'))
 // ReactDOM.render(<NavBar />, document.getElementById('root'))
@@ -21,4 +22,6 @@ ReactDOM.render(<App />, document.getElementById("root"))
 // ReactDOM.render(<LiveLocation />, document.getElementById("root"))
 // ReactDOM.render(<Map />, document.getElementById("root"))
 // ReactDOM.render(<Location />, document.getElementById("root"))
+ReactDOM.render(<UseReducer />, document.getElementById("root"))
+
 
