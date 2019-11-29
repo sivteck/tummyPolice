fd8d8060fe6de95cb9f2a40693f6d4eb101c6104
Sivaram Balakrishnan <sivaram@iyangi.com>
2019-11-29 11:13:20 +0530

    fix merge conflict
    fix handler

--- server/src/Components/DeliveryPartner/deliveryPartnerHandlers.js
+++ server/src/Components/DeliveryPartner/deliveryPartnerHandlers.js
@@ -19,7 +19,6 @@ const login = async (req, res) => {
     const phoneVerification = await verifyDP(phone)
     if (!phoneVerification.username) res.status(200).json({ error: 'invalid phone number' })
     else {
-      const {
       req.session.phone = phone
       req.session.loggedin = true
       req.session.userid = phoneVerification.id
