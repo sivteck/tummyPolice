c0f201051bb548284bdb8cadaaa2b1e083b6ebdd
Sivaram Balakrishnan <sivaram@iyangi.com>
2019-12-09 12:53:41 +0530

    hardcode restaurant id for menu data

--- server/src/db/objectStore/menu.js
+++ server/src/db/objectStore/menu.js
@@ -17,6 +17,8 @@ async function insertMenuItem (restaurantId, itemObj) {
 }
 
 async function getMenuByRestaurantId (restaurantId) {
+  //hardcode restaurant id, since no restaurant specific menu as of now
+  restaurantId = 'f5a53e1d-bd5f-4609-9bb0-12710f96d584'
   let key = 'restaurant:' + restaurantId + ':menu'
   let result = await redis.hgetall(key)
   let menuItems = []
