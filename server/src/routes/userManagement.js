6976334c464fd0c129c513315b8bc08ec4ff1d21
Sivaram Balakrishnan <sivaram@iyangi.com>
2019-12-21 18:01:02 +0530

    fix logout

--- server/src/routes/userManagement.js
+++ server/src/routes/userManagement.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 router.post('/register', register)
 router.post('/login', login)
-router.post('/logout', logout)
+router.get('/logout', logout)
 router.post('/checkValidSession', checkValidSession)
 
 module.exports = router
