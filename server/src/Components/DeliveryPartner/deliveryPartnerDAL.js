175ead43b4aa21c05b3e7da2ec02340677a4d2d0
Sivaram Balakrishnan <sivaram@iyangi.com>
2019-12-25 17:51:25 +0530

    typo :-[

--- server/src/Components/DeliveryPartner/deliveryPartnerDAL.js
+++ server/src/Components/DeliveryPartner/deliveryPartnerDAL.js
@@ -51,7 +51,7 @@ async function updateLocation (id, { latitude, longitude }) {
 async function getNearestDeliveryPartners (restaurantId, radius = 1000) {
   const { location } = getRestaurantInfoById(restaurantId)
   const text = `SELECT * FROM deliverypartners where ST_Distance(ST_GeogFromWKB(location), ST_GeogFromWKB(select location from restaurants where id = $1)) < $2`
-  const values = [restuarantId, radius]
+  const values = [restaurantId, radius]
   try {
     const result = await query(text, values)
     console.log(result.rows, 'result from getNearestDeliveryPartners')
